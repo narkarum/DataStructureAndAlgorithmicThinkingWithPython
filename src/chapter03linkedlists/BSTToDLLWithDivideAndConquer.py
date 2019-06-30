53384fb93adc98498ccf584a34ff4de7ce5724d4
narkarum <narkarum@in.ibm.com>
2019-06-30 16:00:50 +0530

    Update BSTToDLLWithDivideAndConquer.py

--- src/chapter03linkedlists/BSTToDLLWithDivideAndConquer.py
+++ src/chapter03linkedlists/BSTToDLLWithDivideAndConquer.py
@@ -3,7 +3,7 @@
 # Creation Date    		: 2014-01-10 06:15:46 
 # Last modification		: 2008-10-31 
 #               by		: Narasimha Karumanchi 
-# Book Title			: Data Structures And Algorithms Made In Java
+# Book Title			: Data Structures And Algorithmic Thinking with Python
 # Warranty         		: This software is provided "as is" without any 
 # 				   warranty; without even the implied warranty of 
 # 				    merchantability or fitness for a particular purpose. 
